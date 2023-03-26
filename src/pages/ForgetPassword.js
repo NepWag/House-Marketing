660b591473e9cda70b9f1502b7c5c226d4dc5911
Sharosez <sharosez87@gmail.com>
2023-03-26 15:15:25 +0545

    Profile Page and google signin

--- src/pages/ForgetPassword.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Layout from "../components/Layout/Layout";
-
-const ForgetPassword = () => {
-  return <Layout>ForgetPassword</Layout>;
-};
-
-export default ForgetPassword;
